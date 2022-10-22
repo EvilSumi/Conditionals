f30d79d38a444bd777cb10a2f24fbd9314b21fc1
EvilSumi <109980985+EvilSumi@users.noreply.github.com>
2022-10-22 21:26:41 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+int num;
+cin>>num;
+
+if(num%2==0){
+    cout<<"Even"<<endl;
+}
+else{
+    cout<<"Odd"<<endl;
+}
+
+
+
+
+
+
+
+}
