15b73b98cac25c03fc6223bdfdb330fbb57c31d9
EvilSumi <109980985+EvilSumi@users.noreply.github.com>
2022-10-22 21:25:39 +0530

    Add files via upload

---
+++ Agegroup.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+int age;
+cin>>age;
+
+
+if(age<12){
+    cout<<"child"<<endl;
+}
+else if(age>19){
+if(age>65){
+    cout<<"Senior Citizen"<<endl;
+}
+else{
+    cout<<"Adult"<<endl;
+}
+}
+else{
+    cout<<"Teenager"<<endl;
+}
+
+
+
+
+
+
+
+
+}
