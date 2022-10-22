0c1f4d8098daaca4f219570f5c1c0d9214a28308
EvilSumi <109980985+EvilSumi@users.noreply.github.com>
2022-10-22 21:31:25 +0530

    Add files via upload

---
+++ Conditional Operators/Switch/Vowel.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+
+char character;
+cin>>character;
+
+switch(character){
+    case 'a':
+    cout<<"Vowel"<<endl;
+    break;
+   case 'e':
+    cout<<"Vowel"<<endl;
+    break;
+    case 'i':
+    cout<<"Vowel"<<endl;
+    break;
+    case 'o':
+    cout<<"Vowel"<<endl;
+    break;
+    case 'u':
+    cout<<"Vowel"<<endl;
+    break;
+    default:
+    cout<<"Consonant"<<endl;
+    break;
+
+}
+
+
+
+
+
+
+
+
+
+}
