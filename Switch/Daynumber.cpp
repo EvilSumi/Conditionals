8096d68eedcbe1b4b49ae613769cd0724f912215
EvilSumi <109980985+EvilSumi@users.noreply.github.com>
2022-10-22 21:33:58 +0530

    Add files via upload

---
+++ Switch/Daynumber.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+
+int day;
+cin>>day;
+
+switch(day){
+    case 1:
+    cout<<"Monday"<<endl;
+    break;
+    case 2:
+    cout<<"Tueday"<<endl;
+    break;
+    case 3:
+    cout<<"Wednesday"<<endl;
+    break;
+    case 4:
+    cout<<"Thursday"<<endl;
+    break;
+    case 5:
+    cout<<"Friday"<<endl;
+    break;
+    case 6:
+    cout<<"Saturday"<<endl;
+    break;
+    default:
+    cout<<"Sunday"<<endl;
+    break;
+
+}
+
+
+
+
+
+
+
+
+
+}
